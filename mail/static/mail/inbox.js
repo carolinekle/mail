171903ca5c5f4dac47d7334104e981b2e31045cf
carolinekle <caroline.klewinowski@gmail.com>
2023-10-15 17:49:13 -0400

    create send email and inbox. check fro overflow in inbox div

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+
 });
 
 function compose_email() {
@@ -28,20 +29,66 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-  console.log("hey")
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+    all_emails=document.createElement('div')
+    all_emails.classList.add('list-group')
+    document.querySelector('#emails-view').appendChild(all_emails)
+    
+    emails.forEach((email) => {
+      holder = document.createElement('a')
+      holder.classList.add('list-group-item','list-group-item-action', 'flex-column', 'align-items-start')
+      
+      preview = document.createElement('div');
+      preview.classList.add('d-flex', 'w-100', 'justify-content-between')
+      holder.appendChild(preview)
+
+      email_subject = document.createElement('h6');
+      email_subject.classList.add('mb-1')
+      email_subject.innerHTML=`${email.subject}`
+      preview.appendChild(email_subject)
+
+      time = document.createElement('small')
+      time.innerHTML=`${email.timestamp}`
+      preview.appendChild(time)
+
+      text = document.createElement('p');
+      text.classList.add('mb-1')
+      text.innerHTML = `${email.body}`
+      holder.appendChild(text)
+
+      sender = document.createElement('small');
+      sender.classList.add('text-muted')
+      sender.innerHTML = `${email.sender}`
+      holder.appendChild(sender)
+
+      all_emails.appendChild(holder)
+      
+      email.read === true ? holder.classList.add('bg-secondary', 'text-white') : holder.classList.add('bg-light', 'text-dark');
+    
+    }) 
+});
 }
 
 function send_mail(event){
-  console.log("hey");
+  console.log("boo!");
+  recipients=document.querySelector('#compose-recipients').value
+  subject=document.querySelector('#compose-subject').value
+  body=document.querySelector('#compose-body').value
   event.preventDefault();
-/*    fetch('/emails', {
+   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
-        recipients: document.querySelector('#compose-recipients').value,
-        subject: document.querySelector('#compose-subject').value,
-        body: document.querySelector('#compose-body').value
+        recipients: recipients,
+        subject: subject,
+        body: body
     })
   })
   .then(response => response.json())
@@ -49,6 +96,6 @@ function send_mail(event){
       // Print result
       console.log(result);
       load_mailbox(sent)
-  });*/
-  return false; 
+  });
+
 }
