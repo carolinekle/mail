ce0e5392b0db57abfa3ed835c1d767b68cfbd599
carolinekle <caroline.klewinowski@gmail.com>
2023-10-16 15:34:15 -0400

    create email details view and archive functionality. must continue to fix single-view popping up in other view

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
   document.querySelector('#compose-form').addEventListener('submit', send_mail);
-
   // By default, load the inbox
   load_mailbox('inbox');
 
@@ -24,14 +23,99 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+function archive_email(id){
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: true
+    })
+  })
+}
+
+function reply_email(id){
+  console.log(`${id} is listening `)
+}
+
+function view_email(id) {
+  console.log(id)
+  container = document.querySelector('.container')
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+      // Print email
+      console.log(email);
+
+      document.querySelector('#emails-view').style.display = 'none';
+      document.querySelector('#compose-view').style.display = 'none';
+
+      view = document.createElement('div')
+      view.setAttribute('id','single-view')
+      container.appendChild(view)
+
+      emailView = document.createElement("div")
+      emailView.classList.add('jumbotron')
+      view.appendChild(emailView)
+
+      emailSubject = document.createElement('h6')
+      emailSubject.classList.add('display-4')
+      emailSubject.innerHTML = `${email.subject}`
+      emailView.appendChild(emailSubject)
+
+      sender = document.createElement('small')
+      sender.innerHTML = `${email.sender}`
+      emailView.appendChild(sender)
+
+      breakSender = document.createElement('br')
+      emailView.appendChild(breakSender)
+
+      time = document.createElement('small')
+      time.classList.add('my-4')
+      time.innerHTML = `${email.timestamp}`
+      emailView.appendChild(time)
+
+      lineBreak = document.createElement('hr')
+      lineBreak.classList.add('my-4')
+      emailView.appendChild(lineBreak)
+
+      emailBody = document.createElement('p')
+      emailBody.classList.add('lead')
+      emailBody.innerHTML = `${email.body}`
+      emailView.appendChild(emailBody)
+
+      reply = document.createElement('button')
+      reply.classList.add('btn', 'btn-primary','mr-3')
+      reply.innerHTML = 'Reply'
+      view.appendChild(reply)
+      reply.addEventListener('click', () => reply_email(email.id));
+
+      archive = document.createElement('button')
+      archive.classList.add('btn', 'btn-secondary')
+      archive.innerHTML = 'Archive'
+      view.appendChild(archive)
+      archive.addEventListener('click', () => archive_email(email.id));
+
+  });
+
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  })
+  
+  }
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  const singleView = document.querySelector('#single-view');
+  if (singleView) {
+    singleView.style.display = 'none';
+  }
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-
 
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
@@ -45,7 +129,8 @@ function load_mailbox(mailbox) {
     emails.forEach((email) => {
       holder = document.createElement('a')
       holder.classList.add('list-group-item','list-group-item-action', 'flex-column', 'align-items-start')
-      
+      holder.addEventListener('click', () => view_email(email.id));
+
       preview = document.createElement('div');
       preview.classList.add('d-flex', 'w-100', 'justify-content-between')
       holder.appendChild(preview)
@@ -60,10 +145,13 @@ function load_mailbox(mailbox) {
       preview.appendChild(time)
 
       text = document.createElement('p');
-      text.classList.add('mb-1')
-      text.innerHTML = `${email.body}`
-      holder.appendChild(text)
+      if (email.body.length > 100) {
+        text.textContent = `${email.body.substring(0, 120)}...`;
+      } else {
+        text.textContent = `${email.body}`;
+      }
 
+      holder.appendChild(text)
       sender = document.createElement('small');
       sender.classList.add('text-muted')
       sender.innerHTML = `${email.sender}`
@@ -71,14 +159,19 @@ function load_mailbox(mailbox) {
 
       all_emails.appendChild(holder)
       
-      email.read === true ? holder.classList.add('bg-secondary', 'text-white') : holder.classList.add('bg-light', 'text-dark');
+      if (email.read === true) {
+        holder.classList.add('bg-secondary', 'text-white');
+        sender.classList.add('text-white');
+        sender.classList.remove('text-muted');
+      } else {
+        holder.classList.add('bg-light', 'text-dark');
+      }
     
     }) 
 });
 }
 
 function send_mail(event){
-  console.log("boo!");
   recipients=document.querySelector('#compose-recipients').value
   subject=document.querySelector('#compose-subject').value
   body=document.querySelector('#compose-body').value
@@ -99,3 +192,4 @@ function send_mail(event){
   });
 
 }
+
